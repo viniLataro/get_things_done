0e104f67b4dab4c56d36c860f23d198e76585286
Vinicius Lataro <viniciusr.lataro@gmail.com>
2023-04-25 15:22:55 -0300

    add credo into the application

--- mix.exs
+++ mix.exs
@@ -50,7 +50,10 @@ defmodule GetThingsDone.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+
+      # dev
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 
